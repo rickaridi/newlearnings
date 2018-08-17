5d16a342eee42259ec43c34dd5d0d03b0bf9113b
Kiran Krishnamurthy <kiran.krishnamurthy@live.com>
2018-08-17 04:55:32 +0000

    master:

--- src/main/java/io/openshift/booster/Booster.java
+++ src/main/java/io/openshift/booster/Booster.java
@@ -1,4 +1,25 @@
 package io.openshift.booster;                                                      
-public class Booster {                                                      
- public static void main(String[] args) { }                                                      
-} 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@SpringBootApplication(scanBasePackages={"io.openshift"})
+public class Booster {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Booster.class);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> monoRouterFunction(EchoHandler echoHandler) {
+		return route(POST("/echo"), echoHandler::echo).andRoute(GET("/kiran"), echoHandler::kiran);
+	}
+
+}
--- src/test/java/io/openshift/booster/BoosterTest.java
+++ src/test/java/io/openshift/booster/BoosterTest.java
@@ -1,3 +1,38 @@
 package io.openshift.booster;                                                    
 
-public class BoosterTest { } 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import reactor.core.publisher.Mono;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class BoosterTest {
+
+	@Autowired
+	private WebTestClient webClient;
+
+	@Test
+	public void testEcho() {
+		this.webClient.post().uri("/echo").contentType(MediaType.TEXT_PLAIN)
+				.accept(MediaType.TEXT_PLAIN)
+				.body(Mono.just("Hello WebFlux!"), String.class).exchange()
+				.expectBody(String.class).isEqualTo("Hello WebFlux!");
+	}
+
+	@Test
+	public void testKiran() {
+		this.webClient.get().uri("/kiran")
+				.accept(MediaType.TEXT_PLAIN)
+				.exchange();
+	}
+
+
+}
